cf61cb377ff763a3af900f2607f99b16c769287f
Matheus Fernandez <matheus.1994.sm@gmail.com>
2025-02-11 20:34:32 -0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+
+
+print("Bem vindo ao gerador de Nome de Bandas!")
+
+
+
+cidade = input("Qual cidade voce nasceu?\n")
+animal = input("Qual o nome do seu animal de estimação?\n")
+
+print("O nome de sua banda pode ser:\n" + cidade+ " " + animal)
